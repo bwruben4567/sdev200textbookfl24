d405b1266732f3a4a5cfbd2d7bb30bafd3c9ae6e
wmruben27 <bwruben4567@yahoo.com>
2024-08-24 20:59:21 -0400

    trying

--- Chapter2/DataDemo.java
+++ Chapter2/DataDemo.java
@@ -1,6 +1,6 @@
 public class DataDemo {
     public static void main(String[] args) {
-        int aWholeNumber = 311;
+        int aWholeNumber = 315;
         System.out.print("The number is ");
         System.out.println(aWholeNumber);
     }
