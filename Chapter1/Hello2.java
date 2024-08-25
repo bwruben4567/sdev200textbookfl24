487bb332e45a740446227b73c9cf426c243aa9b8
wmruben27 <bwruben4567@yahoo.com>
2024-08-24 20:04:57 -0400

    ch1 files

---
+++ Chapter1/Hello2.java
@@ -0,0 +1,14 @@
+// Filename Hello2.java
+// Written by Bill Ruben
+// Written on 8/21/24
+public class Hello2
+{
+/* This class demonstrates the use of println()
+ method to print the message Hello, world! */ 
+ 
+
+    public static void main(String[] args)
+    {
+        System.out.println("Hello, world!");
+    }
+}
---
+++ Chapter1/HelloDialog.java
@@ -0,0 +1,11 @@
+
+// Filename HelloDialog.java
+// Written by Bill Ruben
+// Written on 8/21/24
+import javax.swing.JOptionPane;
+
+public class HelloDialog {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Hello, world!");
+    }
+}
