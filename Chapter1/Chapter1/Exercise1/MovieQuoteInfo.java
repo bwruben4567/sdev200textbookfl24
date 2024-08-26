a3219a0c71f6668ac73cfc3701ee54df55b128bb
wmruben27 <bwruben4567@yahoo.com>
2024-08-26 16:13:46 -0400

    aug. 26

---
+++ Chapter1/Chapter1/Exercise1/MovieQuoteInfo.java
@@ -0,0 +1,21 @@
+// Filename MovieQuoteInfo.java
+// Written by Wyatt H. Pearce
+// Written on 8-24-2024
+
+package Chapter1.Exercise1;
+
+public class MovieQuoteInfo {
+    /* 
+    This class demonstrates the use of the println() method 
+    to print four messages containing my favorite movie quote, 
+    the movie it comes from, the character who said it, and the year of the movie.
+    */
+
+    public static void main(String[] args)
+    {
+        System.out.println("QUOTE: 'In the beginning, the universe was created. This has made a lot of people very angry and been widely regarded as a bad move.'");
+        System.out.println("MOVIE: The Hitchhiker's Guide to the Galaxy");
+        System.out.println("CHARACTER: The Narrator");
+        System.out.println("MOVIE YEAR: 2005");
+    }
+}
