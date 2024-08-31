78f7d250643369a5343f24410540410f096928c4
wmruben27 <bwruben4567@yahoo.com>
2024-08-31 19:20:09 -0400

    first 3 youdoit ch 3

---
+++ Chapter3/Tutorial3/ParadiseInfo.java
@@ -0,0 +1,12 @@
+// Bill Ruben
+// p.89 Chapter 3 Module 2
+public class ParadiseInfo {
+    public static void main(String[] args) {
+        displayInfo();
+    }
+
+    public static void displayInfo() {
+        System.out.println("Paradise Day Spa wants to pamper you.");
+        System.out.println("We will make you look good.");
+    }
+}
